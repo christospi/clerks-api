4a891564523163b6a1aff505065d9d34a1c410fa
Christos Papaloukas <cpapaloukas@skroutz.gr>
2023-07-29 11:09:55 +0300

    Add `ClerksController` to handle Clerk index action

--- clerks-api/config/routes.rb
+++ clerks-api/config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Routes for "clerks" resource
+  # GET /clerks
+  resources :clerks, only: [:index]
 end
