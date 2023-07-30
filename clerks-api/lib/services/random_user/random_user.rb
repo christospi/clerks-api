777ebbb6b3a4bb305bc8144a6fb6bdea692069f7
Christos Papaloukas <cpapaloukas@skroutz.gr>
2023-07-30 21:01:21 +0300

    Fetch RandomUser data in batches
    Fetch users in batches to avoid overwhelming the API with a
    single large request.

--- clerks-api/lib/services/random_user/random_user.rb
+++ clerks-api/lib/services/random_user/random_user.rb
@@ -6,6 +6,7 @@ module RandomUser
   # Currently we only support the above fields; we can add more if needed.
   ALLOWED_FIELDS = DEFAULT_FIELDS
   DEFAULT_SIZE = 1
+  BATCH_SIZE = 1000
 
   extend self
 
@@ -20,21 +21,35 @@ module RandomUser
   # @option options [Array<String>] :fields The fields to include in the response.
   #   Must be a subset of ALLOWED_FIELDS (default: DEFAULT_FIELDS).
   # @option options [Integer] :size The number of users to fetch (default: DEFAULT_SIZE).
+  # @option options [Integer] :batch_size The number of users to fetch per request (default: BATCH_SIZE).
   # @return [Array<Hash>] An array of hashes representing the users. Each user Hash includes the
   #   requested fields. Returns an empty Array if a request or parsing error occurs.
   #
   # @example Fetch 5 users with the name and email fields:
   #   fetch_users(fields: ['name', 'email'], size: 5)
   #   # => [{'name' => ..., 'email' => ...}, {'name' => ..., 'email' => ...}, ...]
-  def fetch_users(fields: DEFAULT_FIELDS, size: DEFAULT_SIZE)
-    uri = construct_uri(permitted_fields(fields), size)
+  def fetch_users(fields: DEFAULT_FIELDS, size: DEFAULT_SIZE, batch_size: BATCH_SIZE)
+    fields = permitted_fields(fields)
+    total_size = size
+    batch_size = [batch_size, size].min
 
+    users = []
+    while users.size < total_size
+      fetch_size = [total_size - users.size, batch_size].min
+      users.concat(fetch_batch(fields, fetch_size))
+    end
+
+    users
+  end
+
+  private
+
+  def fetch_batch(fields, size)
+    uri = construct_uri(fields, size)
     response = perform_request(uri)
 
     parse_response(response)
   end
-
-  private
 
   def construct_uri(fields, size)
     url = "#{BASE_URL}?results=#{size}"
