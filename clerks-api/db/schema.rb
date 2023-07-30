0a744b4801d12df57e4c6eee1a73f4b6bd76aa97
Christos Papaloukas <cpapaloukas@skroutz.gr>
2023-07-30 13:20:58 +0300

    [Migration] Add `(registration_date, id)` index to Clerk
    Adds a composite index to `clerks` table, consisting of
    `registration_date` and `id`.
    
    As the ordering method of requested records is consistent,
    this index will improve the performance of queries on
    `clerks` table.

--- clerks-api/db/schema.rb
+++ clerks-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_28_165631) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_29_213915) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_28_165631) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_clerks_on_email", unique: true
+    t.index ["registration_date", "id"], name: "index_clerks_on_registration_date_and_id", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
